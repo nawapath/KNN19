77c69590aea4bac33ab8525bcb1ebb0b285d899d
nawapath <664230015@webmail.npru.ac.th>
2025-11-19 15:48:22 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -44,3 +44,10 @@ dt4 = dt['sepalwidth'].sum()
 
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+    #st.write(dt.head(10))
+    st.bar_chart(dx2)
+    st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
