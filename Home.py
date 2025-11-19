c2e59ae471f2d33aea2963caed1f80102a5c1fc2
nawapath <664230015@webmail.npru.ac.th>
2025-11-19 15:42:36 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -22,3 +22,13 @@ with col2:
 with col3:
     st.header("Setosa")
     st.image("./img/iris3.jpg")
+
+
+
+html_7 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
